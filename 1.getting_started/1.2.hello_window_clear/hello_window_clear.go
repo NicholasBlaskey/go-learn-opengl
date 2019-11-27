274e92b9f7edbce9364da9bc9bb7a38408000e2b
Nick Blaskey <nicholas.blaskey1@marist.edu>
2019-11-26 23:16:35 -0500

    Added in hello windows

---
+++ 1.getting_started/1.2.hello_window_clear/hello_window_clear.go
@@ -0,0 +1,72 @@
+// Copied basically statement for statement from
+// https://github.com/cstegel/opengl-samples-golang/blob/master/hello-window/hello_window.go
+
+package main
+
+import(
+	"runtime"
+	"log"
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.1/glfw"
+)
+
+const windowWidth  = 800
+const windowHeight = 600
+
+func init() {
+	runtime.LockOSThread()
+}
+
+func main() {
+	if err := glfw.Init(); err != nil {
+		log.Fatalln("failed to init glfw:", err)
+	}
+	defer glfw.Terminate()
+
+	//glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+	window, err := glfw.CreateWindow(
+		windowWidth, windowHeight, "Hello!", nil, nil)
+
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+
+	// Add in auto resizing
+	window.SetFramebufferSizeCallback(
+		glfw.FramebufferSizeCallback(framebuffer_size_callback))
+	
+	
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	window.SetKeyCallback(keyCallback)
+
+	// Program loop
+	for !window.ShouldClose() {
+		// Poll events and call their registered callbacks
+		glfw.PollEvents()
+
+		gl.ClearColor(0.2, 0.5, 0.5, 1.0)
+		gl.Clear(gl.COLOR_BUFFER_BIT)
+
+		window.SwapBuffers()
+	}
+}
+
+func keyCallback(window *glfw.Window, key glfw.Key, scancode int,
+	action glfw.Action, mods glfw.ModifierKey) {
+	// Escape closes window
+	if key == glfw.KeyEscape && action == glfw.Press {
+		window.SetShouldClose(true)
+	}
+}
+
+func framebuffer_size_callback(w *glfw.Window, width int, height int) {
+	gl.Viewport(0, 0, int32(width), int32(height))
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # go-learn-opengl
+
+Inspired by 
+
+https://learnopengl.com/
+
+https://github.com/JoeyDeVries/LearnOpenGL
+
+Some code taken from
+
+https://github.com/cstegel/opengl-samples-golang
