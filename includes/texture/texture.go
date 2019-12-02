c2743cad1c9d8a9b3428978bac0ea1bef718a1d0
Nick Blaskey <nicholas.blaskey1@marist.edu>
2019-12-02 00:45:11 -0500

    Finished first textures example

---
+++ includes/texture/texture.go
@@ -0,0 +1,34 @@
+package texture
+
+import(
+	"image"
+	"image/draw"
+	"os"
+	 _ "image/jpeg"
+	 _ "image/png"
+)
+
+
+// https://github.com/raedatoui/glutils/blob/master/texture.go
+// taken from
+func ImageLoad(path string) (*image.RGBA) {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		panic(err)
+	}
+
+	rgba := image.NewRGBA(img.Bounds())
+	if rgba.Stride != rgba.Rect.Size().X * 4 {
+		panic("Unsupported stride")
+	}
+
+	draw.Draw(rgba, rgba.Bounds(), img, image.Point{0, 0}, draw.Src)
+	
+	return rgba
+}
