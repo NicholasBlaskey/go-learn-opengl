d7a72d317bd9b8d6e280b2a007dc5f732d331fbb
Nick Blaskey <nicholas.blaskey1@marist.edu>
2020-01-08 01:23:22 -0500

    Got loadModel working and started working on the processNode function

--- includes/model/model.go
+++ includes/model/model.go
@@ -1,15 +1,36 @@
 // translated from
 
+// used a lot of cgo stuff from
+// https://github.com/tbogdala/assimp-go/blob/master/assimp.go
+
 package model
 
+/*
+#cgo CPPFLAGS: -I/mingw64/include -std=c99
+#cgo LDFLAGS: -L/mingw64/lib -lassimp -lz -lstdc++
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <assimp/cimport.h>
+#include <assimp/scene.h>
+#include <assimp/mesh.h>
+#include <assimp/cimport.h>
+#include <assimp/matrix4x4.h>
+#include <assimp/postprocess.h>
+*/
+import "C"
+
 import(
-	//"log"
+	"log"
 	"unsafe"
-	"strconv"
+	//"strconv"
 	"strings"
 	
-	"github.com/go-gl/mathgl/mgl32"
-	"github.com/go-gl/gl/v4.1-core/gl"
+	//"github.com/go-gl/mathgl/mgl32"
+	//"github.com/go-gl/gl/v4.1-core/gl"
 
 	"github.com/nicholasblaskey/go-learn-opengl/includes/mesh"
 	"github.com/nicholasblaskey/go-learn-opengl/includes/shader"
@@ -22,7 +43,7 @@ type Model struct {
 	gammaCorrection bool
 }
 
-func newModel(path string, gamma bool) *Model {
+func NewModel(path string, gamma bool) *Model {
 	model := Model{gammaCorrection: gamma}
 	model.loadModel(path)
 
@@ -36,25 +57,47 @@ func (model *Model) Draw(shader shader.Shader) {
 }
 
 func (model *Model) loadModel(path string) {
-	/*
-	// read file via ASSIMP
-	Assimp::Importer importer;
-	const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
-	// check for errors
-	if(!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) // if is Not Zero
-	{
-		cout << "ERROR::ASSIMP:: " << importer.GetErrorString() << endl;
-		return;
-	}
-	*/
+	cPathString := C.CString(path)
+	defer C.free(unsafe.Pointer(cPathString))
 
+	scene := C.aiImportFile(cPathString,
+		C.aiProcess_Triangulate |
+		C.aiProcess_FlipUVs |
+		C.aiProcess_CalcTangentSpace)
+
+	//log.Println(scene)
+	//log.Printf("%T\n", scene)
+	log.Printf("%+v", scene)
+	
+	// Make sure we loaded meshes properly
+	if uintptr(unsafe.Pointer(scene)) == 0 {
+		panic("filepath: " + path + "loaded a nil scene")
+	}
+	if scene.mNumMeshes < 1 {
+		panic("Got zero meshes when loading")
+	}
+	if uintptr(unsafe.Pointer(scene.mRootNode)) == 0 {
+		panic("Root node of the scene was nil")
+	}
+	
+	
 	// Retrieve the directory of the filepath
 	model.directory = path[0:strings.LastIndex(path, "/")]
 
-	// processNode(scene->mRootNode, scene);
+	model.processNode(scene.mRootNode, scene)	
 }
 
-func (model *Model) processNode(aiNode *node, aiScene *scene) {
+func (model *Model) processNode(aiNode *C.struct_aiNode,
+	aiScene *C.struct_aiScene) {
+	
+	for i := 0; i < int(aiNode.mNumMeshes); i++ {
+		log.Println(i)
+	}
+	for i := 0; i < int(aiNode.mNumChildren); i++ {
+		log.Printf("%T\n", aiNode.mChildren)
+		//model.processNode(aiNode.mChildren, aiScene)
+	}
+}
 	/*
 	// process each mesh located at the current node
 	for(unsigned int i = 0; i < node->mNumMeshes; i++)
@@ -70,7 +113,7 @@ func (model *Model) processNode(aiNode *node, aiScene *scene) {
 		processNode(node->mChildren[i], scene);
 	}
 	*/
-}
+//}
 
 // processMesh
 
---
+++ includes/modelT.go
@@ -0,0 +1,9 @@
+package main
+
+import(
+	"github.com/nicholasblaskey/go-learn-opengl/includes/model"
+)
+
+func main() {
+	model.NewModel("../resources/objects/nanosuit/nanosuit.obj", false)
+}
