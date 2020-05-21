a2689545948a4a39769eaa7c9ceb1c309528b5bf
Nick Blaskey <nicholas.blaskey1@marist.edu>
2020-05-20 21:45:12 -0400

    Added in the second geometry shader example

---
+++ src/4.advanced_opengl/9.2.geometry_shader_exploding/geometry_shader_exploding.go
@@ -0,0 +1,202 @@
+// Translated from
+// https://github.com/JoeyDeVries/LearnOpenGL/blob/master/src/2.lighting/1.colors/colors.cpp
+
+package main
+
+import(
+	"runtime"
+	"log"
+	
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.1/glfw"
+	"github.com/go-gl/mathgl/mgl32"
+
+	"github.com/nicholasblaskey/go-learn-opengl/includes/shader"
+	"github.com/nicholasblaskey/go-learn-opengl/includes/camera"
+	loadModel "github.com/nicholasblaskey/go-learn-opengl/includes/model"
+)
+
+// Settings
+const windowWidth  = 800
+const windowHeight = 600
+
+// Camera
+var ourCamera camera.Camera = camera.NewCamera(
+	0.0, 0.0, 3.0, // pos xyz
+	0.0, 1.0, 0.0, // up xyz
+	-90.0, 0.0,    // Yaw and pitch
+	80.0, 45.0, 0.1)   // Speed, zoom, and mouse sensitivity 
+var firstMouse bool = true
+var lastX float32   = windowWidth / 2
+var lastY float32   = windowHeight / 2
+
+// Timing
+var deltaTime float32 = 0.0
+var lastFrame float32 = 0.0
+
+// Lighting
+var lightPos mgl32.Vec3 = mgl32.Vec3{1.2, 1.0, 2.0}
+
+// Controls
+var heldW bool = false;
+var heldA bool = false;
+var heldS bool = false;
+var heldD bool = false;
+
+func init() {
+	runtime.LockOSThread()
+}
+
+func initGLFW() *glfw.Window { 
+    if err := glfw.Init(); err != nil {
+        log.Fatalln("failed to init glfw:", err)
+    }
+
+    //glfw.WindowHint(glfw.Resizable, glfw.False)
+    glfw.WindowHint(glfw.ContextVersionMajor, 4)
+    glfw.WindowHint(glfw.ContextVersionMinor, 1)
+    glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+    glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+    window, err := glfw.CreateWindow(
+        windowWidth, windowHeight, "Hello!", nil, nil)
+
+    if err != nil {
+        panic(err)
+    }
+    window.MakeContextCurrent()
+
+    // Add in auto resizing
+    window.SetFramebufferSizeCallback(
+        glfw.FramebufferSizeCallback(framebuffer_size_callback))
+    window.SetCursorPosCallback(glfw.CursorPosCallback(mouse_callback))
+    window.SetScrollCallback(glfw.ScrollCallback(scroll_callback))
+
+    // Tell glfw to capture the mouse
+    window.SetInputMode(glfw.CursorMode, glfw.CursorDisabled)
+
+	window.SetKeyCallback(keyCallback)
+
+	
+    if err := gl.Init(); err != nil {
+        panic(err)
+    }
+
+    // Config gl global state
+    gl.Enable(gl.DEPTH_TEST)
+
+	return window
+}
+
+func main() {
+	window := initGLFW()
+	defer glfw.Terminate()
+	
+	ourShader := shader.MakeGeomShaders("9.2.geometry_shader.vs",
+		"9.2.geometry_shader.fs", "9.2.geometry_shader.gs")
+	ourModel := loadModel.NewModel(
+		"../../../resources/objects/nanosuit/nanosuit.obj", false)
+	
+	// Draw in polygon mode
+	//gl.PolygonMode(gl.FRONT_AND_BACK, gl.LINE)
+	
+	// Program loop
+	for !window.ShouldClose() {
+		// Pre frame logic
+		currentFrame := float32(glfw.GetTime())
+		deltaTime = currentFrame - lastFrame
+		lastFrame = currentFrame
+			
+		// Poll events and call their registered callbacks
+		glfw.PollEvents()
+		
+		gl.ClearColor(0.05, 0.05, 0.05, 1.0)
+		gl.Clear(gl.COLOR_BUFFER_BIT)
+		gl.Clear(gl.DEPTH_BUFFER_BIT)
+		
+		ourShader.Use()
+		
+		// View / projection transformations
+		projection := mgl32.Perspective(mgl32.DegToRad(ourCamera.Zoom),
+			float32(windowHeight) / windowWidth, 0.1, 100.0)
+		view := ourCamera.GetViewMatrix()
+		ourShader.SetMat4("projection", projection)
+		ourShader.SetMat4("view", view)
+
+		// Render the model
+		model := mgl32.Translate3D(0.0, -1.75, 0)
+		model = model.Mul4(mgl32.Scale3D(0.2, 0.2, 0.2))
+		ourShader.SetMat4("model", model)
+
+		// Add time component to geo shader
+		ourShader.SetFloat("time", float32(glfw.GetTime()))
+
+		ourModel.Draw(ourShader)
+		window.SwapBuffers()
+		
+	}
+}
+
+func keyCallback(window *glfw.Window, key glfw.Key, scancode int,
+	action glfw.Action, mods glfw.ModifierKey) {
+	
+	// Escape closes window
+	if key == glfw.KeyEscape && action == glfw.Press {
+		window.SetShouldClose(true)
+	}
+ 
+	if key == glfw.KeyW && action == glfw.Press || heldW {
+		ourCamera.ProcessKeyboard(camera.FORWARD, deltaTime)
+		heldW = true
+	}
+	if key == glfw.KeyS && action == glfw.Press || heldS {
+		ourCamera.ProcessKeyboard(camera.BACKWARD, deltaTime)
+		heldS = true
+	}
+	if key == glfw.KeyA && action == glfw.Press || heldA {
+		ourCamera.ProcessKeyboard(camera.LEFT, deltaTime)
+		heldA = true
+	}
+	if key == glfw.KeyD && action == glfw.Press || heldD {
+		ourCamera.ProcessKeyboard(camera.RIGHT, deltaTime)
+		heldD = true
+	}
+
+	if key == glfw.KeyW && action == glfw.Release {
+		heldW = false
+	}
+	if key == glfw.KeyS && action == glfw.Release {
+		heldS = false
+	}
+	if key == glfw.KeyA && action == glfw.Release {
+		heldA = false
+	}
+	if key == glfw.KeyD && action == glfw.Release {
+		heldD = false
+	}
+}
+
+func mouse_callback(w *glfw.Window, xPos float64, yPos float64) {
+	if firstMouse {
+		lastX = float32(xPos)
+		lastY = float32(yPos)
+		firstMouse = false
+	}
+
+	xOffset := float32(xPos) - lastX
+	// Reversed due to y coords go from bot up
+	yOffset := lastY - float32(yPos)
+
+	lastX = float32(xPos)
+	lastY = float32(yPos)
+	
+	ourCamera.ProcessMouseMovement(xOffset, yOffset, true)
+}
+
+func scroll_callback(w *glfw.Window, xOffset float64, yOffset float64) {
+	ourCamera.ProcessMouseScroll(float32(yOffset))
+}
+
+func framebuffer_size_callback(w *glfw.Window, width int, height int) {
+	gl.Viewport(0, 0, int32(width), int32(height))
+}
+
