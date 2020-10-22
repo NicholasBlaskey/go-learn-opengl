4812ed5aa3ce0cf62e1c344ae7591469739ee1c5
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2020-10-21 23:58:08 -0400

    Added in the game object

---
+++ src/7.in_practice/3.2d_game/0.full_source/gameObject/gameObject.go
@@ -0,0 +1,34 @@
+package gameObject
+
+import (
+	"github.com/go-gl/mathgl/mgl32"
+
+	"github.com/nicholasblaskey/go-learn-opengl/src/7.in_practice/3.2d_game/0.full_source/spriteRenderer"
+	"github.com/nicholasblaskey/go-learn-opengl/src/7.in_practice/3.2d_game/0.full_source/texture"
+)
+
+type GameObject struct {
+	Position  mgl32.Vec2
+	Size      mgl32.Vec2
+	Velocity  mgl32.Vec2
+	Color     float32
+	Rotation  float32
+	Sprite    *texture.Texture
+	IsSolid   bool
+	Destroyed bool
+}
+
+func New(pos, size mgl32.Vec2, sprite *texture.Texture,
+	color float32, velocity mgl32.Vec2) *GameObject {
+
+	return &GameObject{pos, size, velocity, color, 0.0, sprite, false, false}
+}
+
+func NewDefault() *GameObject {
+	return &GameObject{Size: mgl32.Vec2{1, 1}, Color: 1.0}
+}
+
+func (g *GameObject) Draw(sr *spriteRenderer.SpriteRenderer) {
+	sr.DrawSprite(g.Sprite, g.Position, g.Size, g.Rotation,
+		mgl32.Vec3{g.Color, g.Color, g.Color})
+}
